0e38889d47b2f2c9dd085daa937d9cf3286e469c
Robert French <178658682+rfrench3@users.noreply.github.com>
2026-07-04 21:57:41 +0000

    remove inaccurate test outputs

--- tests/update.sh
+++ tests/update.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/sh
 # Simulate real-ish output from commands
-# in: exact output, out: already parsed output (not perfect, but more data)
 
 SCRIPT_DIR="$(dirname $0)"
 
@@ -9,9 +8,3 @@ sleep 1
 
 echo "INPUT TEXT UUPD 1"
 cat "$SCRIPT_DIR/uupd_in_1.txt"
-
-echo "OUTPUT TEXT UUPD 1"
-cat "$SCRIPT_DIR/uupd_out_1.txt"
-
-echo "OUTPUT TEXT UUPD 2"
-cat "$SCRIPT_DIR/uupd_out_2.txt"
